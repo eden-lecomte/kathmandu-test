976f3e0c463ae503ac67d17f006a17bf380a0c8f
edenlc@gmail.com <eden.lecomte@ccc.govt.nz>
2020-11-10 21:55:20 +1300

    add additional API requests

--- util/fetch.js
+++ util/fetch.js
@@ -1,14 +1,33 @@
+import { storeData } from "../redux/actions";
+import { store } from "../redux/store";
 
-export async function getRestaurants(filter) {
+// Search for restaurants based on Redux state filters
+export async function getRestaurants() {
 
+  // Clear current restaurant when fetching
+  store.dispatch(storeData('', 'restaurant'));
+  const state = store.getState();
+
+  // Set query params to pass to Zomato
   const queryParameters = {
-    city_id: 90, // Christchurch
+    entity_id: 90, // Christchurch
     entity_type: 'city',
     start: 0,
     count: 100,
   };
+
+  // Apply filters if applicable
+  if (state.cuisine && state.cuisine.length > 0) {
+    queryParameters.cuisines = state.cuisine.join(',');
+  };
+  if (state.categories && state.categories.length > 0) {
+    queryParameters.category = state.categories.join(',');
+  };
+
+  // Encode URL params
   const encodedParams = new URLSearchParams(queryParameters).toString();
 
+  // Construct URL for request
   const url = `https://developers.zomato.com/api/v2.1/search?${encodedParams}`;
 
   const response = await fetch(url, {
@@ -18,12 +37,11 @@ export async function getRestaurants(filter) {
     },
   });
   const json = await response.json()
-  console.log(json);
 
   return json;
 }
 
-
+// Fetch Zomato categories
 export async function getCategories() {
 
   const url = `https://developers.zomato.com/api/v2.1/categories`;
@@ -37,3 +55,44 @@ export async function getCategories() {
   const json = await response.json()
   return json;
 }
+
+// Fetch Zomtato Cuisine
+export async function getCuisine() {
+
+  const queryParameters = {
+    city_id: 90, // Christchurch
+  };
+  const encodedParams = new URLSearchParams(queryParameters).toString();
+
+  const url = `https://developers.zomato.com/api/v2.1/cuisines?${encodedParams}`;
+
+  const response = await fetch(url, {
+    headers: {
+      'user-key': 'ce9f0554d9f7c7a812bf7c1b75e29372',
+      'Content-Type': 'application/jsonp'
+    },
+  });
+  const json = await response.json()
+  return json;
+}
+
+// Get details of a single restaurant by ID
+export async function getRestaurant(id) {
+
+  const queryParameters = {
+    res_id: id,
+  };
+  const encodedParams = new URLSearchParams(queryParameters).toString();
+
+  const url = `https://developers.zomato.com/api/v2.1/restaurant?${encodedParams}`;
+
+  const response = await fetch(url, {
+    headers: {
+      'user-key': 'ce9f0554d9f7c7a812bf7c1b75e29372',
+      'Content-Type': 'application/jsonp'
+    },
+  });
+  const json = await response.json()
+  return json;
+}
+
